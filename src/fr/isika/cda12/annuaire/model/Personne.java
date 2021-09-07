09bccada9e42478a37b06ff7c4de1db54cf6dd89
tititanou <tania.rojas@edu.itescia.fr>
2021-09-07 17:40:27 +0200

    Classe Personne modifiée

--- src/fr/isika/cda12/annuaire/model/Personne.java
+++ src/fr/isika/cda12/annuaire/model/Personne.java
@@ -4,39 +4,36 @@ public class Personne {
 	
 	private String nom;
 	private String prenom;
-	//private String id;
+	private String id;
+	private String mdp;
+	
+	public Personne(String nom, String prenom, String mdp) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.id = nom + "_" + prenom;
+		this.mdp = mdp;
+	}
 	
 	public String getNom() {
 		return nom;
 	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+	
 	public String getPrenom() {
 		return prenom;
 	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-//	public String getId() {
-//		return id;
-//	}
-//	public void setId(String id) {
-//		this.id = id;
-//	}
-	public Personne(String nom, String prenom) {
-		this.nom = nom;
-		this.prenom = prenom;	
+	
+	public String getId() {
+		return id;
 	}
 	
-	public Personne() {
-		
+	public String getMdp() {
+		return mdp;
 	}
+	
 	
 	public boolean creerCompte ( String lastName, String firstName, String pwd1, String pwd2) {
 		
 		boolean result;
-		Personne person = new Personne (lastName, firstName);
 		
 		if (pwd1.length() != pwd2.length()) {
 			System.out.println("Les mots de passe sont différents");
@@ -47,6 +44,7 @@ public class Personne {
 		    if (pwd1.equals(pwd2)) {
 		    	System.out.println("le compte est créé");
 		    	result = true;
+		    	Personne person = new Personne (lastName, firstName, pwd1);
 		    } else {
 		    	System.out.println("Les mots de passe sont différents");
 		    	result = false;
@@ -73,7 +71,7 @@ public class Personne {
 	@Override
 	public String toString() {
 		
-		return this.nom + " " + this.prenom;
+		return this.nom + "," + this.prenom + "," + this.id + "," + this.mdp;
 	}
 
 }
